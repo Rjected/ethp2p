55759963eb7d5a87e5f2647776553d97c55c2ac1
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-06-14 16:53:48 -0400

    change NodeData Bytes type and derive RLP traits

--- src/state.rs
+++ src/state.rs
@@ -1,12 +1,11 @@
-use ethers::types::Bytes;
+use fastrlp::{RlpEncodable, RlpDecodable};
 
 /// A request for state tree nodes corresponding to the given hashes.
 pub struct GetNodeData(Vec<[u8; 32]>);
-
-// TODO: Bytes fastrlp encoding / decoding
 
 /// The response to [GetNodeData](crate::GetNodeData), containing the state tree nodes or contract
 /// bytecode corresponding to the requested hashes.
 ///
 /// Not all nodes are guaranteed to be returned by the peer.
-pub struct NodeData(Vec<Bytes>);
+#[derive(Clone, Debug, RlpEncodable, RlpDecodable)]
+pub struct NodeData(Vec<bytes::Bytes>);
