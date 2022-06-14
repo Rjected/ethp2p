5d1156f35351c47559f3e8f1c0a49cf531636a00
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-06-14 19:46:25 -0400

    derive Encodable and Decodable for GetNodeData
     * Add PartialEq and Eq to NodeData

--- src/state.rs
+++ src/state.rs
@@ -1,11 +1,12 @@
 use fastrlp::{RlpEncodable, RlpDecodable};
 
 /// A request for state tree nodes corresponding to the given hashes.
+#[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
 pub struct GetNodeData(Vec<[u8; 32]>);
 
 /// The response to [GetNodeData](crate::GetNodeData), containing the state tree nodes or contract
 /// bytecode corresponding to the requested hashes.
 ///
 /// Not all nodes are guaranteed to be returned by the peer.
-#[derive(Clone, Debug, RlpEncodable, RlpDecodable)]
+#[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
 pub struct NodeData(Vec<bytes::Bytes>);
