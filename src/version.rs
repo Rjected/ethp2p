732fb4c2b6e091c462b4b5f6d7a8be4590a05ad3
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-06-25 03:12:06 -0400

    add eth65 to version

--- src/version.rs
+++ src/version.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, Hash, Serialize, Deserialize, PartialEq, Eq)]
 pub enum EthVersion {
+    Eth65 = 65,
     Eth66 = 66,
     Eth67 = 67,
 }
