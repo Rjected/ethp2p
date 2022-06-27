632e527d39e0ed70054cfc2d0437fd9112e055d9
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-06-27 18:31:25 -0400

    impl From for EthMessage variants

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,30 @@ pub use state::{GetNodeData, NodeData};
 
 mod version;
 pub use version::EthVersion;
+
+// impl from for each variant of EthMessage
+macro_rules! message_from_impl {
+    ($t:ty, $variant:ident) => {
+        impl From<$t> for EthMessage {
+            fn from(t: $t) -> Self {
+                EthMessage::$variant(t)
+            }
+        }
+    }
+}
+
+message_from_impl!(Status, Status);
+message_from_impl!(NewBlockHashes, NewBlockHashes);
+message_from_impl!(NewBlock, NewBlock);
+message_from_impl!(Transactions, Transactions);
+message_from_impl!(NewPooledTransactionHashes, NewPooledTransactionHashes);
+message_from_impl!(RequestPair<GetBlockHeaders>, GetBlockHeaders);
+message_from_impl!(RequestPair<BlockHeaders>, BlockHeaders);
+message_from_impl!(RequestPair<GetBlockBodies>, GetBlockBodies);
+message_from_impl!(RequestPair<BlockBodies>, BlockBodies);
+message_from_impl!(RequestPair<GetPooledTransactions>, GetPooledTransactions);
+message_from_impl!(RequestPair<PooledTransactions>, PooledTransactions);
+message_from_impl!(RequestPair<GetNodeData>, GetNodeData);
+message_from_impl!(RequestPair<NodeData>, NodeData);
+message_from_impl!(RequestPair<GetReceipts>, GetReceipts);
+message_from_impl!(RequestPair<Receipts>, Receipts);
