714251f40924e630fec219d386d99cc7575259e6
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-06-27 02:16:55 -0400

    add network tests and fix encoding/decoding

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,5 +1,4 @@
 use anvil_core::eth::transaction::TypedTransaction;
-use ethers::core::types::transaction::eip2930::AccessList;
 use fastrlp::{
     length_of_length, Decodable, Encodable, Header, RlpDecodableWrapper, RlpEncodableWrapper,
 };
@@ -22,7 +21,15 @@ impl PooledTransactions {
     pub(crate) fn pooled_payload_length(&self) -> usize {
         let mut length = 0;
         for tx_response in &self.0 {
-            length += tx_response.length();
+            length += match tx_response {
+                TypedTransaction::Legacy(tx) => tx.length(),
+                TypedTransaction::EIP2930(tx) => {
+                    length_of_length(tx.length()) + tx.length() + 1
+                }
+                TypedTransaction::EIP1559(tx) => {
+                    length_of_length(tx.length()) + tx.length() + 1
+                }
+            }
         }
         length
     }
@@ -47,19 +54,21 @@ impl Encodable for PooledTransactions {
                 TypedTransaction::EIP2930(tx) => {
                     let tx_header = Header {
                         list: false,
-                        payload_length: tx.length(),
+                        payload_length: tx.length() + 1,
                     };
 
                     tx_header.encode(out);
+                    out.put_u8(0x01);
                     tx.encode(out);
                 }
                 TypedTransaction::EIP1559(tx) => {
                     let tx_header = Header {
                         list: false,
-                        payload_length: tx.length(),
+                        payload_length: tx.length() + 1,
                     };
 
                     tx_header.encode(out);
+                    out.put_u8(0x02);
                     tx.encode(out);
                 }
             }
@@ -343,5 +352,90 @@ mod test {
         }
 
         assert_eq!(decoded_transactions, expected_transactions);
+    }
+
+    #[test]
+    fn encode_pooled_transactions_network() {
+        let expected = hex!("f9022980f90225f8650f84832156008287fb94cf7f9e66af820a19257a2108375b180b0ec491678204d2802ca035b7bfeb9ad9ece2cbafaaf8e202e706b4cfaeb233f46198f00b44d4a566a981a0612638fb29427ca33b9a3be2a0a561beecfe0269655be160d35e72d366a6a860b87502f872041a8459682f008459682f0d8252089461815774383099e24810ab832a5b2a5425c154d58829a2241af62c000080c001a059e6b67f48fb32e7e570dfb11e042b5ad2e55e3ce3ce9cd989c7e06e07feeafda0016b83f4f980694ed2eee4d10667242b1f40dc406901b34125b008d334d47469f86b0384773594008398968094d3e8763675e4c425df46cc3b5c0f6cbdac39604687038d7ea4c68000802ba0ce6834447c0a4193c40382e6c57ae33b241379c5418caac9cdc18d786fd12071a03ca3ae86580e94550d7c071e3a02eadb5a77830947c9225165cf9100901bee88f86b01843b9aca00830186a094d3e8763675e4c425df46cc3b5c0f6cbdac3960468702769bb01b2a00802ba0e24d8bd32ad906d6f8b8d7741e08d1959df021698b19ee232feba15361587d0aa05406ad177223213df262cb66ccbb2f46bfdccfdfbbb5ffdda9e2c02d977631daf86b02843b9aca00830186a094d3e8763675e4c425df46cc3b5c0f6cbdac39604687038d7ea4c68000802ba00eb96ca19e8a77102767a41fc85a36afd5c61ccb09911cec5d3e86e193d9c5aea03a456401896b1b6055311536bf00a718568c744d8c1f9df59879e8350220ca18");
+
+        let transactions = RequestPair::<PooledTransactions> {
+            request_id: 0,
+            message: vec![
+                TypedTransaction::Legacy(LegacyTransaction {
+                    nonce: 15u64.into(),
+                    gas_price: 2200000000u64.into(),
+                    gas_limit: 34811u64.into(),
+                    kind: TransactionKind::Call(hex!("cf7f9e66af820a19257a2108375b180b0ec49167").into()),
+                    value: 1234u64.into(),
+                    input: Bytes::default(),
+                    signature: Signature {
+                        v: 44,
+                        r: U256::from_str("35b7bfeb9ad9ece2cbafaaf8e202e706b4cfaeb233f46198f00b44d4a566a981").unwrap(),
+                        s: U256::from_str("612638fb29427ca33b9a3be2a0a561beecfe0269655be160d35e72d366a6a860").unwrap(),
+                    },
+                }),
+                TypedTransaction::EIP1559(EIP1559Transaction {
+                    chain_id: 4,
+                    nonce: 26u64.into(),
+                    max_priority_fee_per_gas: 1500000000u64.into(),
+                    max_fee_per_gas: 1500000013u64.into(),
+                    gas_limit: 21000u64.into(),
+                    kind: TransactionKind::Call(hex!("61815774383099e24810ab832a5b2a5425c154d5").into()),
+                    value: 3000000000000000000u64.into(),
+                    input: Bytes::default(),
+                    access_list: AccessList::default(),
+                    odd_y_parity: true,
+                    r: H256::from_str("59e6b67f48fb32e7e570dfb11e042b5ad2e55e3ce3ce9cd989c7e06e07feeafd").unwrap(),
+                    s: H256::from_str("016b83f4f980694ed2eee4d10667242b1f40dc406901b34125b008d334d47469").unwrap(),
+                }),
+                TypedTransaction::Legacy(LegacyTransaction {
+                    nonce: 3u64.into(),
+                    gas_price: 2000000000u64.into(),
+                    gas_limit: 10000000u64.into(),
+                    kind: TransactionKind::Call(hex!("d3e8763675e4c425df46cc3b5c0f6cbdac396046").into()),
+                    value: 1000000000000000u64.into(),
+                    input: Bytes::default(),
+                    signature: Signature {
+                        v: 43,
+                        r: U256::from_str("ce6834447c0a4193c40382e6c57ae33b241379c5418caac9cdc18d786fd12071").unwrap(),
+                        s: U256::from_str("3ca3ae86580e94550d7c071e3a02eadb5a77830947c9225165cf9100901bee88").unwrap(),
+                    },
+                }),
+                TypedTransaction::Legacy(LegacyTransaction {
+                    nonce: 1u64.into(),
+                    gas_price: 1000000000u64.into(),
+                    gas_limit: 100000u64.into(),
+                    kind: TransactionKind::Call(hex!("d3e8763675e4c425df46cc3b5c0f6cbdac396046").into()),
+                    value: 693361000000000u64.into(),
+                    input: Bytes::default(),
+                    signature: Signature {
+                        v: 43,
+                        r: U256::from_str("e24d8bd32ad906d6f8b8d7741e08d1959df021698b19ee232feba15361587d0a").unwrap(),
+                        s: U256::from_str("5406ad177223213df262cb66ccbb2f46bfdccfdfbbb5ffdda9e2c02d977631da").unwrap(),
+                    },
+                }),
+                TypedTransaction::Legacy(LegacyTransaction {
+                    nonce: 2u64.into(),
+                    gas_price: 1000000000u64.into(),
+                    gas_limit: 100000u64.into(),
+                    kind: TransactionKind::Call(hex!("d3e8763675e4c425df46cc3b5c0f6cbdac396046").into()),
+                    value: 1000000000000000u64.into(),
+                    input: Bytes::default(),
+                    signature: Signature {
+                        v: 43,
+                        r: U256::from_str("eb96ca19e8a77102767a41fc85a36afd5c61ccb09911cec5d3e86e193d9c5ae").unwrap(),
+                        s: U256::from_str("3a456401896b1b6055311536bf00a718568c744d8c1f9df59879e8350220ca18").unwrap(),
+                    },
+                }),
+            ].into(),
+        };
+
+        let mut encoded = vec![];
+        transactions.encode(&mut encoded);
+        assert_eq!(encoded.len(), transactions.length());
+        let encoded_str = hex::encode(&encoded);
+        let expected_str = hex::encode(&expected);
+        assert_eq!(encoded_str.len(), expected_str.len());
+        assert_eq!(encoded_str, expected_str);
     }
 }
