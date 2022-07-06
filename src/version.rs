8cbe86eaddaad714135a73e86e3eb3700ca9f211
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-07-06 13:28:19 -0400

    remove Eth65 from version
     * `Eth65` should only be re-added if there is support for `eth/65`
       style messages. Currently `EthMessage` is `eth/66` style, using
       `RequestPair` for request-response messages.

--- src/version.rs
+++ src/version.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, Hash, Serialize, Deserialize, PartialEq, Eq)]
 pub enum EthVersion {
-    Eth65 = 65,
     Eth66 = 66,
     Eth67 = 67,
 }
