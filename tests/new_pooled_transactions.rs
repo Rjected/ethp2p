8bea09d502abfdbb6fe0ce9d32b2ec2182365d50
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-07-12 21:39:39 -0400

    test NewBlock decoding
     * add some blocks collected from bsc devp2p
     * fixes #9

--- tests/new_pooled_transactions.rs
+++ tests/new_pooled_transactions.rs
@@ -1,11 +1,10 @@
-//! Encoding and decoding tests for [`NewPooledTransactions`]
+//! Decoding tests for [`NewPooledTransactions`]
 use ethp2p_rs::NewPooledTransactionHashes;
 use fastrlp::Decodable;
 use std::{fs, path::PathBuf};
 
 #[test]
 fn decode_new_pooled_transaction_hashes_network() {
-    // large test vector
     let network_data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("testdata/new_pooled_transactions_network_rlp");
     let data = fs::read_to_string(network_data_path).expect("Unable to read file");
