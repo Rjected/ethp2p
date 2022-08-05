b70311c34df3d9b5c2689617b43160865b35ad16
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-08-04 23:17:36 -0400

    feat: impl TryFrom<String> for EthVersion

--- src/version.rs
+++ src/version.rs
@@ -6,3 +6,15 @@ pub enum EthVersion {
     Eth66 = 66,
     Eth67 = 67,
 }
+
+impl TryFrom<String> for EthVersion {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.as_str() {
+            "66" => Ok(EthVersion::Eth66),
+            "67" => Ok(EthVersion::Eth67),
+            _ => Err(format!("Unknown version: {}", s)),
+        }
+    }
+}
